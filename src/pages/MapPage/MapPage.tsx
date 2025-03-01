92567b518ecad14e86b9a716c47767c49bb67799
Ewrdad <Ewrdad12+git@gmail.com>
2025-03-01 12:50:54 +0000

    template structure

---
+++ src/pages/MapPage/MapPage.tsx
@@ -0,0 +1,13 @@
+import { Sidebar } from "./Sidebar/Sidebar";
+import { Map } from "./Map/Map";
+import { Footer } from "./Footer/Footer";
+
+export const MapPage = () => {
+  return (
+    <div>
+      <Sidebar />
+      <Map />
+      <Footer />
+    </div>
+  );
+};

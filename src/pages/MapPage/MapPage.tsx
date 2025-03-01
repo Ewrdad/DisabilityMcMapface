d59d6c1b7ffca0c5e603b9389f2944d994389dac
Ewrdad <Ewrdad12+git@gmail.com>
2025-03-01 16:26:35 +0000

    Added import

--- src/pages/MapPage/MapPage.tsx
+++ src/pages/MapPage/MapPage.tsx
@@ -2,12 +2,14 @@ import { Sidebar } from "./Sidebar/Sidebar";
 import { Map } from "./Map/Map";
 import { Footer } from "./Footer/Footer";
 
+import { useState } from "react";
+
 export const MapPage = () => {
-  const [filters, setFilters] = useState(["somethign"])
+  const [filters, setFilters] = useState(["somethign"]);
   return (
     <div>
-      <Sidebar filters setFilters() />
-      <Map filters />
+      <Sidebar />
+      <Map />
       <Footer />
     </div>
   );

ee3e02873d65bfd29b98cb9ebba648d6629ceb0a
Dylan Jones <dyl@anjon.es>
2025-03-01 16:49:50 +0000

    Wired together

--- src/pages/MapPage/Map/Map.tsx
+++ src/pages/MapPage/Map/Map.tsx
@@ -1,21 +1,16 @@
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import Route from "./Route";
 import { LatLngLiteral } from "leaflet";
-import { useState } from "react";
 import { Tag } from "../../../Types";
 
 const source: LatLngLiteral = { lat: 51.4921091, lng: -3.1835541 };
 const destination: LatLngLiteral = { lat: 51.4823, lng: -3.1812 };
 
-export const Map = () => {
-
-  const [filters, setFilters] = useState<Tag[]>([]);
+export const Map = ({ filters }: {filters: Tag[]}) => {
 
   return (
     <div>
       <h1>Map</h1>
-      <button onClick={() => setFilters(["wheelchair"])}>wheelchair</button>
-      <button onClick={() => setFilters(["noise"])}>noise</button>
       <MapContainer className="h-128 w-128" center={source} zoom={17} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
--- src/pages/MapPage/MapPage.tsx
+++ src/pages/MapPage/MapPage.tsx
@@ -10,7 +10,7 @@ export const MapPage = () => {
   return (
     <div>
       <Sidebar setFilters={setFilters} />
-      <Map />
+      <Map filters={filters} />
       <Footer />
     </div>
   );

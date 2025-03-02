45518c9c454d3bf6b1ebd6e62ba6775d894ebb39
Dylan Jones <dyl@anjon.es>
2025-03-02 12:52:34 +0000

    Move gh action

--- src/pages/MapPage/Sidebar/ClosedSidebar/ClosedSidebar.tsx
+++ src/pages/MapPage/Sidebar/ClosedSidebar/ClosedSidebar.tsx
@@ -6,11 +6,11 @@ export const ClosedSidebar = ({ open }: { open: () => void }) => {
     <div className="absoloute top-0 left-0 h-screen w-6">
       <Button
         onClick={() => open()}
-        className="bg-linear-to-t to-sky-500 from-pink-500 content-left fixed top-0 left-0 h-full w-full  "
+        className="bg-linear-to-t to-sky-500 from-pink-500 content-left fixed top-0 left-0 h-full w-full"
       >
         {" "}
-        <div className="object-center align-middle ">
-          <MenuIcon className="object-center pt-2 self-start text-white " />
+        <div className="object-center align-middle">
+          <MenuIcon className="object-center pt-2 self-start text-white" />
         </div>
       </Button>
     </div>
